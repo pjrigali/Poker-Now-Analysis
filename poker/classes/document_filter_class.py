ec7a34bda3c97e29f6d58e5c3ffde71ec580ef46
Peter <prigali>
2022-05-24 16:23:01 -0500

    Rebuilt DocumentFilter

--- poker/classes/document_filter_class.py
+++ poker/classes/document_filter_class.py
@@ -1,21 +1,158 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Tuple
 from dataclasses import dataclass
 import pandas as pd
-from poker.classes.poker_class import Poker
-from poker.utils.base import flatten
-pd.set_option('use_inf_as_na', True)
+from poker.classes.poker import Poker
+from poker.utils.class_functions import _get_attributes
 
 
-def _df_convert_list(data: Union[List[str], str, None]) -> Union[List[str], None]:
+def _df_convert_list(data: Union[List[str], Tuple[str], str, None]) -> Union[List[str], None]:
     """Converts str to List[str] and raises error"""
-    if type(data) == list:
-        return data
-    elif data is None:
+    if data is None:
         return None
-    elif type(data) == str:
+    elif isinstance(data, list):
+        return data
+    elif isinstance(data, tuple):
+        return list(data)
+    elif isinstance(data, str):
         return [data]
     else:
         raise AttributeError('Incorrect input type used')
+
+
+def _game_id(val: Union[List[str], str, None], data: Poker) -> dict:
+    match_names = {k: True for k, v in data.matches.items()}
+    if val is None:
+        dic = match_names
+    elif isinstance(val, str):
+        if val in match_names:
+            dic = {val: True}
+    elif isinstance(val, list):
+        dic = {}
+        for i in val:
+            if i in match_names:
+                dic[i] = True
+    else:
+        raise AttributeError('')
+    return dic
+
+
+def _player_index(val: Union[List[str], str, None], data: Poker) -> dict:
+    indexes = {}
+    for k, v in data.players.items():
+        for i in v.player_indexes:
+            indexes[i] = True
+    if val is None:
+        dic = indexes
+    elif isinstance(val, str):
+        if val in indexes:
+            dic = {val: True}
+    elif isinstance(val, list):
+        dic = {}
+        for i in val:
+            if i in indexes:
+                dic[i] = True
+    else:
+        raise AttributeError('')
+    return dic
+
+
+def _event(val: Union[List[str], str, None]) -> dict:
+    events = {'Calls': True, 'Checks': True, 'Folds': True, 'Bets': True, 'Shows': True, 'Wins': True,
+              'PlayerStacks': True, 'BigBlind': True, 'SmallBlind': True, 'Flop': True, 'Turn': True,
+              'River': True, 'MyCards': True, 'Joined': True, 'Requests': True, 'Approved': True,
+              'Quits': True, 'Undealt': True, 'StandsUp': True, 'SitsIn': True}
+    if val is None:
+        dic = events
+    elif isinstance(val, str):
+        if val in events:
+            dic = {val: True}
+    elif isinstance(val, list):
+        dic = {}
+        for i in val:
+            if i in events:
+                dic[i] = True
+    else:
+        raise AttributeError('')
+    return dic
+
+
+def _position(val: Union[List[str], str, None]) -> dict:
+    positions = {'Pre Flop': True, 'Post Flop': True, 'Post Turn': True, 'Post River': True}
+    if val is None:
+        dic = positions
+    elif isinstance(val, str):
+        if val in positions:
+            dic = {val: True}
+    elif isinstance(val, list):
+        dic = {}
+        for i in val:
+            if i in positions:
+                dic[i] = True
+    else:
+        raise AttributeError('')
+    return dic
+
+
+def _win_loss_all(val: Union[List[str], str, None]) -> tuple:
+    if val == 'Win':
+        return (True,)
+    elif val == 'Loss':
+        return (None,)
+    else:
+        return (True, None)
+
+
+def _check(val, dic) -> bool:
+    if val in dic:
+        return True
+    else:
+        return False
+
+
+def _get_data(data: Poker, game_id: list, player_index: list, event: list, position: list, win_loss_all: str) -> list:
+    game_id = _game_id(val=game_id, data=data)
+    player_index = _player_index(val=player_index, data=data)
+    event = _event(val=event)
+    position = _position(val=position)
+    win_loss_all = _win_loss_all(val=win_loss_all)
+
+    table_event = None
+    if 'PlayerStacks' in event:
+        table_event = True
+    elif 'Flop' in event:
+        table_event = True
+    elif 'Turn' in event:
+        table_event = True
+    elif 'River' in event:
+        table_event = True
+    elif 'Undealt' in event:
+        table_event = True
+
+    lst = []
+    for e in data.events:
+        if _check(e.game_id, game_id) is False:
+            continue
+        elif _check(e.event, event) is False:
+            continue
+        elif _check(e.position, position) is False:
+            continue
+        elif e.wins not in win_loss_all:
+            continue
+        if table_event is not None and _check(e.event, event):
+            lst.append((e.time, e))
+            continue
+        if isinstance(e.player_index, str):
+            if _check(e.player_index, player_index) is False:
+                continue
+        elif isinstance(e.player_index, (list, tuple)):
+            for i in e.player_index:
+                if _check(e.player_index, player_index):
+                    lst.append((e.time, e))
+                    break
+            continue
+        lst.append((e.time, e))
+    lst = sorted(lst, key=lambda x: x[0])
+    return [i[1] for i in lst]
 
 
 @dataclass
@@ -27,208 +164,43 @@ class DocumentFilter:
 
     :param data: Input Poker object to be filtered.
     :type data: Poker
-    :param game_id_lst: Game Id filter, default is None. *Optional*
-    :type game_id_lst: Union[List[str], str, None]
-    :param player_index_lst: Player Index filter, default is None. *Optional*
-    :type player_index_lst: Union[List[str], str, None]
-    :param player_name_lst: Player Name filter, default is None. *Optional*
-    :type player_name_lst: Union[List[str], str, None]
-    :param class_lst: Filter by class objects, default is None. *Optional*
-    :type class_lst: Union[List[str], str, None]
-    :param position_lst: Filter by position, default is None. *Optional*
-    :type position_lst: Union[List[str], str, None]
+    :param game_id: Game Id filter, default is None. *Optional*
+    :type game_id: Union[List[str], str, None]
+    :param player_index: Player Index filter, default is None. *Optional*
+    :type player_index: Union[List[str], str, None]
+    :param player_name: Player Name filter, default is None. *Optional*
+    :type player_name: Union[List[str], str, None]
+    :param event: Filter by class objects, default is None. *Optional*
+    :type event: Union[List[str], str, None]
+    :param position: Filter by position, default is None. *Optional*
+    :type position: Union[List[str], str, None]
     :param win_loss_all: Filter by Win, Loss or All, default is None. *Optional*
     :type win_loss_all: Union[str, None]
-    :param column_lst: Filter by column name, default is None. *Optional*
-    :type column_lst: Union[List[str], str, None]
     :example: *None*
     :note: All inputs, except data, are *Optional* and defaults are set to None.
         Any str inputs are placed in a list.
 
     """
+
+    __slots__ = ('game_id', 'player_index', 'event', 'position', 'win_loss_all', 'data')
+
     def __init__(self, data: Poker,
-                 game_id_lst: Optional[Union[List[str], str, None]] = None,
-                 player_index_lst: Optional[Union[List[str], str, None]] = None,
-                 player_name_lst: Optional[Union[List[str], str, None]] = None,
-                 class_lst: Optional[Union[List[str], str, None]] = None,
-                 position_lst: Optional[Union[List[str], str, None]] = None,
-                 win_loss_all: Optional[str] = None,
-                 column_lst: Optional[Union[List[str], str, None]] = None):
-        self._data = data
-        self._game_id_lst = _df_convert_list(data=game_id_lst)
-        self._player_index_lst = _df_convert_list(data=player_index_lst)
-        self._player_name_lst = _df_convert_list(data=player_name_lst)
-        self._class_lst = _df_convert_list(data=class_lst)
-        self._position_lst = _df_convert_list(data=position_lst)
-        self._win_loss_all = win_loss_all
-        self._column_lst = _df_convert_list(data=column_lst)
-
-        if self._win_loss_all is None:
-            self._win_loss_all = 'All'
-        elif self._win_loss_all in ['Win', 'Loss', 'All']:
-            pass
-        else:
-            raise AttributeError('Incorrect win_loss_all str used')
-
-        dic = {}
-        for key, val in self._data.players_history.items():
-            for key1, val1 in val.merged_moves[self._win_loss_all].items():
-                if self._column_lst is None:
-                    if key1 in dic.keys():
-                        dic[key1] += val1
-                    else:
-                        dic[key1] = val1
-                elif self._column_lst is not None and key1 in self._column_lst:
-                    if key1 in dic.keys():
-                        dic[key1] += val1
-                    else:
-                        dic[key1] = val1
-                elif self._column_lst is not None and key1 not in self._column_lst:
-                    raise AttributeError('Column not in the dictionary')
-
-        col_lst = []
-        item_dic = {}
-        if self._game_id_lst is not None:
-            col_lst.append('Game Id')
-            for item in self._game_id_lst:
-                item_dic[item] = True
-        if self._player_index_lst is not None:
-            col_lst.append('Player Index')
-            for item in self._player_index_lst:
-                item_dic[item] = True
-        if self._player_name_lst is not None:
-            col_lst.append('Player Name')
-            for item in self._player_name_lst:
-                item_dic[item] = True
-        if self._position_lst is not None:
-            col_lst.append('Position')
-            for item in self._position_lst:
-                item_dic[item] = True
-        if self._class_lst is not None:
-            col_lst.append('Class')
-            for item in self._class_lst:
-                item_dic[item] = True
-
-        col_lst_len = len(col_lst)
-        if len(col_lst) > 0:
-            ind_set = None
-            temp_ind_dic = {col: {} for col in col_lst}
-            for col in col_lst:
-                if col not in ['Player Name', 'Player Index']:
-                    temp_ind_dic[col] = {ind: True for ind, item in enumerate(dic[col]) if item in item_dic}
-                else:
-                    temp_values = [item if type(item) == list else [item] for item in dic[col]]
-                    temp = [[item for item in flatten(item_lst, 'str')] for item_lst in temp_values]
-                    for ind, item_lst in enumerate(temp):
-                        for item in item_lst:
-                            if item in item_dic:
-                                temp_ind_dic[col][ind] = True
-                                break
-                if ind_set is None:
-                    ind_set = set(temp_ind_dic[col].keys())
-                else:
-                    (ind_set.add(item) for item in temp_ind_dic[col].keys())
-
-            final_ind = []
-            for ind in ind_set:
-                count = 0
-                for col in col_lst:
-                    if ind in temp_ind_dic[col]:
-                        count += 1
-                if count == col_lst_len:
-                    final_ind.append(ind)
-            final_ind.sort()
-
-            for key, val in dic.items():
-                dic[key] = [val[ind] for ind in final_ind]
-
-        temp_class_bool_player = False
-        if self._class_lst is not None and 'Player Stacks' not in self._class_lst:
-            temp_class_bool_player = True
-        elif self._class_lst is None:
-            temp_class_bool_player = True
-
-        final_df = pd.DataFrame(dic).drop_duplicates('Time', keep='first').sort_values('Time', ascending=True).reset_index(drop=True)
-        if 'Player Index' in final_df.columns and temp_class_bool_player is True:
-            psc, pcc = [], []
-            for ind, row in final_df.iterrows():
-                if ind == 0:
-                    if type(row['Player Index']) == str:
-                        prev = row['Player Index']
-
-                if row['Class'] != 'Player Stacks':
-                    psc.append(row['Player Starting Chips'])
-                    pcc.append(row['Player Current Chips'])
-                else:
-                    val = row['Player Index'].index(prev)
-                    psc.append(row['Player Starting Chips'][val])
-                    pcc.append(row['Player Current Chips'][val])
-
-                if type(row['Player Index']) == str:
-                    prev = row['Player Index']
-
-            final_df['Player Starting Chips'], final_df['Player Current Chips'] = psc, pcc
-        final_df['Seconds'] = [(row['Time'] - row['Previous Time']).total_seconds() for ind, row in final_df.iterrows()]
-        final_df['Seconds into Hand'] = [(row['Time'] - row['Start Time']).total_seconds() for ind, row in final_df.iterrows()]
-
-        if 'Class' in final_df.columns and 'Start Time' in final_df.columns:
-            pos_dic = {'Pre Flop': 1, 'Post Flop': 2, 'Post Turn': 3, 'Post River': 4, 'None': 5}
-            start_lst = final_df['Start Time'].tolist()
-            pos_lst = final_df['Position'].tolist()
-            f_dic = dict(zip(final_df[final_df['Class'] == 'Folds']['Start Time'].tolist(),
-                             final_df[final_df['Class'] == 'Folds']['Position'].tolist()))
-            st_f_dic = {i: True for i in f_dic.keys()}
-            f_p_lst = [f_dic[item] if item in st_f_dic else 'None' for ind, item in enumerate(start_lst)]
-            fold_next_lst = []
-            for ind, item in enumerate(pos_lst):
-                if pos_dic[item] + 1 == pos_dic[f_p_lst[ind]] and start_lst[ind] in st_f_dic:
-                    fold_next_lst.append(1)
-                else:
-                    fold_next_lst.append(0)
-            final_df['Fold Next Position'] = fold_next_lst
-            final_df['Fold Position'] = f_p_lst
-
-        self._df = final_df
+                 game_id: Optional[Union[List[str], str, None]] = None,
+                 player_index: Optional[Union[List[str], str, None]] = None,
+                 event: Optional[Union[List[str], str, None]] = None,
+                 position: Optional[Union[List[str], str, None]] = None,
+                 win_loss_all: Optional[str] = None):
+        self.game_id = _df_convert_list(data=game_id)
+        self.player_index = _df_convert_list(data=player_index)
+        self.event = _df_convert_list(data=event)
+        self.position = _df_convert_list(data=position)
+        self.win_loss_all = win_loss_all
+        self.data = _get_data(data=data, game_id=self.game_id, player_index=self.player_index, event=self.event,
+                              position=self.position, win_loss_all=self.win_loss_all)
 
     def __repr__(self):
         return "DocumentFilter"
 
-    @property
     def df(self) -> pd.DataFrame:
-        """Returns a DataFrame of requested items"""
-        return self._df
+        return pd.DataFrame.from_dict([_get_attributes(i) for i in self.data])
 
-    @property
-    def game_id_lst(self) -> Union[List[str], None]:
-        """Returns game id input"""
-        return self._game_id_lst
-
-    @property
-    def player_index_lst(self) -> Union[List[str], None]:
-        """Returns player index input"""
-        return self._player_index_lst
-
-    @property
-    def player_name_lst(self) -> Union[List[str], None]:
-        """Returns player name input"""
-        return self._player_name_lst
-
-    @property
-    def class_lst(self) -> Union[List[str], None]:
-        """Returns class input"""
-        return self._class_lst
-
-    @property
-    def position_lst(self) -> Union[List[str], None]:
-        """Returns position input"""
-        return self._position_lst
-
-    @property
-    def win_loss_all(self) -> Union[str, None]:
-        """Returns win loss or all input"""
-        return self._win_loss_all
-
-    @property
-    def column_lst(self) -> Union[List[str], None]:
-        """Returns column input"""
-        return self._column_lst
