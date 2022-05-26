35d01cbbe8ad55282bdbd750b383d6028009f811
Peter <prigali>
2022-05-26 02:44:15 -0500

    adds dict handling functions

--- poker/utils/class_functions.py
+++ poker/utils/class_functions.py
@@ -16,7 +16,7 @@ def _get_attributes(val) -> dict:
 
 def _get_keys(dic: dict) -> tuple:
     """Creates tuple of keys"""
-    return tuple([k for k, v in dic.items()])
+    return tuple(k for k, v in dic.items())
 
 
 def _get_percent(w_num: Union[int, float], h_num: Union[int, float], ret: Optional[float] = 0.0) -> float:
@@ -33,3 +33,28 @@ def _get_percent_change(new: Union[int, float], old: Union[int, float], ret: Opt
         return round((new - old) / old, 2)
     else:
         return ret
+
+
+def tdict(k: Union[list, tuple], v=True) -> dict:
+    """Creates a dict with a specific value"""
+    if isinstance(v, bool):
+        return {i: True for i in k}
+    elif isinstance(v, list):
+        return {i: [] for i in k}
+    elif isinstance(v, dict):
+        return {i: {} for i in k}
+
+
+def _dict(val, d: dict, kv: str = 'k') -> dict:
+    """Checks if a value is in a true dict and adds the value if it isnt"""
+    if _str_nan(val) and val not in d:
+        return {**d, **{val: True}}
+    elif isinstance(val, (list, tuple)):
+        return {**d, **{i: True for i in val if i not in d}}
+    elif isinstance(val, dict):
+        if kv == 'k':
+            return {**d, **{k for k, v in val.items() if k not in d}}
+        else:
+            return {**d, **{v for k, v in val.items() if k not in d}}
+    else:
+        return d
